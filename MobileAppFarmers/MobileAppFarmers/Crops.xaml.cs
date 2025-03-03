3643fe9497d472cee4d9eee557d68a97633308a3
Bpin0102s <nepal0009777@gmail.com>
2025-03-03 13:30:41 -0500

    added collections and crops

---
+++ MobileAppFarmers/MobileAppFarmers/Crops.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppFarmers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Crops : ContentPage
+    {
+        public Crops()
+        {
+            InitializeComponent();
+        }
+    }
+}
